077702d67ae4168b3aeaded6965476d970bb9727
Roman <theredballpoint@gmail.com>
2017-10-31 19:07:09 +0300

    well added

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ var proxy = require('express-http-proxy');
 var date = require('node-datetime');
 var moment = require('moment');
 
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use( express.static(__dirname + '/node_modules/jquery/dist'));
+app.use(express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
 app.use(express.static(__dirname+'/static'));
 
 moment.locale('ru');
